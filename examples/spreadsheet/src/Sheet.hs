b1ef56e1ec3094addf46ad096b4a46e05d4103b5
Gregers Rørdam <gregers@rordam.dk>
2023-04-24 16:06:21 +0200

    WIP: sheet and selectMany

--- examples/spreadsheet/src/Sheet.hs
+++ examples/spreadsheet/src/Sheet.hs
@@ -2,16 +2,52 @@ module Sheet where
 
 import Rattus.Channels
 import qualified Rattus.Primitives as Prim
+import Rattus.Primitives (box, unbox, select, delay, adv)
+import qualified Rattus.Stream as Stream
+import qualified Rattus.Later as Later
+import qualified Rattus.Strict as Strict
 import qualified Data.Set
+import qualified Data.Map as Map
 
-data Input = IntValue Int | CharValue Char | MouseValue Bool
+import Expr
+
+type Cell = String
+data CellUpdate = NewFormula Expr | UpdatedDependency (Cell, Maybe Int)
+
+type Input = CellFormula
 type O a = Prim.O Input a
-
+type Str a = Stream.Str Input a
 
 -- Identifier, value of cell, dependent on these cells
 --type Cell f = (String, f, O Int)
---type Spreadsheet = Map String Cell 
+--type Spreadsheet = Map String Cell
 
+cells = [(x, y) | x <- ['A' .. 'E'], y <- [1 .. 5]]
 
-(input, inputMaybe, depend, [kbChannel, mouseChanel]) = mkChannels ["keyboard", "mouse"]
+-- Produces strings "A1", "A2", ... "B1", ... "E5"
+cellStrings = map (\(x,y) -> [x] ++ show y) cells
+
+(input, inputMaybe, depend, channels) = mkChannels cellStrings
+
+cellToChannel = Map.fromList $ zip cellStrings channels
+
+-- Given a list of cell streams, produces as stream of corresponding variable environments
+varEnv :: Strict.List (O (Str (Cell, Int))) -> O (Str VarEnv)
+varEnv delayedStreams = scanAwait (box fold) emptyVarEnv delayedStreams
+    where
+        laterChangedCells = Later.selectMany delayedStreams
+        strChangedCells = Stream.fromLater laterChangedCells
+        fold acc (cell, value) = Map.insert cell value acc
+
+cell :: Expr -> O (Str VarEnv) -> O (Str (Maybe Int))
+cell e envs = Later.map (Stream.map (evalMaybe e)) envs
+
+-- 1st arg: initial variable environment
+-- 2nd arg: Stream of cell updates
+resettableCell :: O (Str VarEnv) -> O (Str CellUpdate) -> O (Str (Maybe Int))
+resettableCell varEnvs updates = delay (
+        case adv updates of
+            NewFormula e ::: us -> 
+            UpdatedDependency (cellId, maybeValue) ::: us ->
+    )
 
--- src/Rattus/Later.hs
+++ src/Rattus/Later.hs
@@ -1,15 +1,37 @@
 {-# OPTIONS -fplugin=Rattus.Plugin #-}
 
 module Rattus.Later (
-    map
+    map,
+    selectMany,
 ) where
 
-import Prelude hiding (map)
+import Prelude hiding (map, Left, Right)
 import Rattus
-import Rattus.Primitives ( O, delay, adv )
+import Rattus.Primitives ( O, delay, adv, Select(..))
+import Rattus.Strict (List(..), singleton)
 
 {-# ANN module Rattus #-}
 
 map :: (a -> b) -> O v a -> O v b
 map f later = delay (f (adv later))
 
+-- Given a list of delayed values, select over them return list of all available values when one arrives.
+selectMany :: List (O v a) -> O v (List (Int, a))
+selectMany lst = aux 0 lst
+    where
+        aux n (x :! y :! Nil) = 
+            delay (
+                case select x y of
+                    Both a b -> (n, a) :! (n+1, b) :! Nil
+                    Left a lb -> singleton (n, a)
+                    Right la b -> singleton (n+1, b)
+            )
+        aux n (x :! xs) =
+            delay (
+                let xs' = (aux (n+1) xs) in
+                case select x xs' of
+                    Both a b -> (n, a) :! b
+                    Left a lb -> singleton (n, a)
+                    Right la b -> b
+            )
+        
