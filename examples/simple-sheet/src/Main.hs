f5d82b9e16ea7f018849782d878ee845f1b15dbb
Gregers Rørdam <gregers@rordam.dk>
2023-04-27 15:43:23 +0200

    Clean up simple-sheet

--- examples/simple-sheet/src/Main.hs
+++ examples/simple-sheet/src/Main.hs
@@ -11,63 +11,32 @@ import MegaParser
 import Expr
 import qualified Data.Map as Map
 import Data.Map (Map)
-import Sheet hiding (O, Stream)
+import Data.Functor ((<&>))
+import Sheet
+import Text.Read (readMaybe)
 
-type O a = Prim.O Int a
-type Stream a = Stream.Str Int a
+input' :: String -> Int -> O a -> Maybe a
+input' chId = if chId `elem` inputCells
+              then inputMaybe chId
+              else \_ _ -> Nothing
 
 processLine :: (Show a) => (String, O (Stream a)) -> String -> (String, O (Stream a))
 processLine (_, later) line =
     case parseLine line of
-        Just (cellId, Number i) ->
-            case inputMaybe cellId i later of
+        Just (cellId, i) ->
+            case input' cellId i later of
                 Just (x ::: xs) -> (show x, xs)
                 Nothing -> ("That cell does not affect the output cells", later)
-        _ -> ("Could not parse input as a valid expression.", later)
+        Nothing -> ("Could not parse input as a valid expression.", later)
 
-parseLine :: String -> Maybe (String, Expr)
+parseLine :: String -> Maybe (String, Int)
 parseLine line = do
     case words line of
-        cellId : "=" : first : rest -> (cellId,) <$> parse (unwords (first : rest))
+        [cellId, "=", num] -> (readMaybe num :: Maybe Int) <&> (cellId,)
         _ -> Nothing
 
 main :: IO ()
 main = do
-    {-
-    let a = parse "a1 * 10"
-    let b = parse "a1 /             10 + -10 "
-    let c = parse "a1 * 60 + 10"
-    let d = parse "60 * 10 - 100"
-    let e = parse "60 + 10 * 100"
-    print a 
-    print b
-    print c
-    print $ a >>= \res -> evalMaybe res emptyVarEnv
-    print $ b >>= \res -> evalMaybe res emptyVarEnv
-    print $ c >>= \res -> evalMaybe res emptyVarEnv
-    print $ d >>= \res -> evalMaybe res emptyVarEnv
-    print $ e >>= \res -> evalMaybe res emptyVarEnv
-
-    let (env Stream.::: envStr) = input "A3" ("A2", Number 5) testVarEnv
-    let (env2 Stream.::: envStr2) = input "A3" ("A2", Number 3) envStr
-    print env
-    print env2
-    let testCell = cell (Times (Var "A1") (Number 10)) testVarEnv
-    let (cellValue Stream.::: cellValues) = input "A2" ("A2", Number 4) testCell
-    let (cellValue2 Stream.::: cellValues2) = input "A1" ("A1", Number 6) cellValues
-    print cellValue
-    print cellValue2
--}
-    --------------------------------
-
     let (initial ::: miniSheet2) = miniSheet
-    print initial {-
-    let (t ::: miniSheet3) = input "A1" 5 miniSheet2
-    print t
-    let (t2 ::: miniSheet4) = input "B1" 9 miniSheet3
-    print t2
-    let (t3 ::: miniSheet5) = input "B2" 10 miniSheet4
-    print t3 -}
-
-
+    print initial
     interact (unlines . map fst . scanl processLine ("", miniSheet2) . lines)
